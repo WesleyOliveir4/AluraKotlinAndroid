e0448537b9b4f883a78b732f45fd87fa94fdd3cb
wesley oliveira <wlwwesley9@gmail.com>
2023-11-10 21:36:04 -0300

    Buscando notas da API

--- android-com-kotlin-comunicacao-web-projeto-inicial/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++ android-com-kotlin-comunicacao-web-projeto-inicial/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 class RetrofitInicializador {
 
     val retrofit = Retrofit.Builder()
-        .baseUrl("http://localhost:8080/")
+        .baseUrl("http://192.168.2.100:8080/")
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
