03cb857544e489dbf3b74aa9a458e55ca12a39c7
wesley oliveira <wlwwesley9@gmail.com>
2023-12-10 17:10:01 -0300

    UUID

---
+++ android-com-kotlin-comunicacao-web-projeto-inicial/app/src/main/java/br/com/alura/ceep/migrations/Migrations.kt
@@ -0,0 +1,10 @@
+package br.com.alura.ceep.migrations
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+val MIGRATION_1_2 = object : Migration(1,2){
+    override fun migrate(database: SupportSQLiteDatabase) {
+        TODO("Not yet implemented")
+    }
+}
